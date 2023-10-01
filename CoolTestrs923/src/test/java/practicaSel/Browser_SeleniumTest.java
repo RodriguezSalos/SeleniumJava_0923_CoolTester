716ea617f7ecb26544d20bcbe91afeae0f1eeb72
salorod <salorod@localhost>
2023-10-01 06:36:32 -0500

    Update to proper name of the test class

--- CoolTestrs923/src/test/java/practicaSel/Browser_SeleniumTest.java
+++ CoolTestrs923/src/test/java/practicaSel/Browser_SeleniumTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
 
-public class NewTest {
+public class Browser_SeleniumTest {
   @Test
   public void TestChrome() {
 		System.out.println("Test Chrome");
